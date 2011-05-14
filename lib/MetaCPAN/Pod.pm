3cdf6d5e3edcba70858c954ddece1e1732ef24f7
Olaf Alders <olaf@wundersolutions.com>
2011-05-14 10:19:29 -0400

    Implements caching layer for file fetching.

--- lib/MetaCPAN/Pod.pm
+++ lib/MetaCPAN/Pod.pm
@@ -2,10 +2,14 @@ package MetaCPAN::Pod;
 
 use Moose;
 
+use CHI;
 use Data::Dump qw( dump );
 use Furl;
 use JSON;
 use MetaCPAN::Pod::XHTML;
+use WWW::Mechanize::Cached;
+
+has 'mech' => ( is => 'rw', lazy_build => 1 );
 
 sub convert {
 
@@ -18,20 +22,18 @@ sub convert {
     );
 
     my $res = $furl->get( "http://api.beta.metacpan.org/module/$name" );
-
     die $res->status_line unless $res->is_success;
 
     my $module = from_json( $res->content );
 
-    $res = $furl->get(
+    my $file_res = $self->mech->get(
         sprintf(
             "http://api.beta.metacpan.org/source/%s/%s/%s",
             $module->{author}, $module->{release}, $module->{path}
         )
     );
     
-    die $res->status_line unless $res->is_success;
-    my $pod = $res->content;
+    die $file_res->status_line unless $file_res->is_success;
 
     my $parser = MetaCPAN::Pod::XHTML->new();
 
@@ -44,12 +46,23 @@ sub convert {
 
     my $html = "";
     $parser->output_string( \$html );
-    $parser->parse_string_document( $pod );
+    $parser->parse_string_document( $file_res->content );
 
     die "no content" if !$parser->content_seen;
 
     return $html;
 
+}
+
+sub _build_mech {
+    
+    my $cache = CHI->new(
+        driver   => 'File',
+        root_dir => '/tmp/pod2html'
+    );
+    
+    return WWW::Mechanize::Cached->new( cache => $cache );   
+    
 }
 
 1;
